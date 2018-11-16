89e11933e34012ab25e3e7206091243da29822d2
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-16 08:16:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,15 @@ class Student
   end
   
   def self.create_table
-    
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
   
 end
